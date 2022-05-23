406e8f343e3e9a98670b9c746d4a33f276533d80
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2022-05-23 11:36:15 +0200

    proper onCollision function added

--- src/hero.ts
+++ src/hero.ts
@@ -1,23 +1,23 @@
-import * as PIXI from "pixi.js"
-import {Game} from "./game"
+import * as PIXI from "pixi.js";
+import { Texture } from "pixi.js";
+import { Game } from "./game";
 
 export class Hero extends PIXI.AnimatedSprite {
+  private game: Game;
+  //geef aan hoe en snel de enemy is ook de positie waar de zombie is word hier aangegeven
+  constructor(game: Game, textures: Texture[]) {
+    console.log("I'm a hero");
+    super(textures);
+    this.game = game;
 
-    private game: Game
-    //geef aan hoe en snel de enemy is ook de positie waar de zombie is word hier aangegeven
-    constructor(game: Game, textures) {
-        console.log("I'm a zombie")
-        super(textures)
-        this.game = game
+    this.anchor.set(0.5);
+    this.x = 1100;
+    this.y = 400;
+    this.animationSpeed = 0.1;
+    this.loop = true;
+    this.play();
 
-        this.anchor.set(0.5)
-        this.x = 1100
-        this.y = 400
-        this.animationSpeed = 0.1
-        this.loop = true
-        this.play()
-
-        //voeg de enemy aan het beeld toe
-        this.game.pixi.stage.addChild(this)
-    }
+    //voeg de enemy aan het beeld toe
+    this.game.pixi.stage.addChild(this);
+  }
 }
